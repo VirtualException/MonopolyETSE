59cffb8ff5ea09a120b24be105297d898eb45338
davidblanc00 <david.blanco.pena@rai.usc.es>
2024-09-26 11:40:01 +0200

    MODIFICACION Tablero.java

--- src/monopoly/Tablero.java
+++ src/monopoly/Tablero.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
+
+
 public class Tablero {
     //Atributos.
     private ArrayList<ArrayList<Casilla>> posiciones; //Posiciones del tablero: se define como un arraylist de arraylists de casillas (uno por cada lado del tablero).
@@ -27,15 +29,15 @@ public class Tablero {
     //Método para insertar las casillas del lado norte.
     private void insertarLadoNorte() {
         ArrayList<Casilla> ladoNorte = new ArrayList<>();
-        ladoNorte.add(new Casilla("Solar12","Solar",22,(2.2f*Valor.FORTUNA_INICIAL),banca));
+        ladoNorte.add(new Casilla("Solar12","Solar",22,Valor.VALOR_GRUPO_VERMELLO/3,banca));
         ladoNorte.add(new Casilla("Suerte","Suerte",23,banca));
-        ladoNorte.add(new Casilla("Solar13","Solar",24,(2.2f*Valor.FORTUNA_INICIAL),banca));
-        ladoNorte.add(new Casilla("Solar14","Solar",25,(2.2f*Valor.FORTUNA_INICIAL),banca));
-        ladoNorte.add(new Casilla("Trans3","Transporte",26,Valor.SUMA_VUELTA,banca));
-        ladoNorte.add(new Casilla("Solar15","Solar",27,(2.5f*Valor.FORTUNA_INICIAL),banca));
-        ladoNorte.add(new Casilla("Solar16","Solar",28,(2.5f*Valor.FORTUNA_INICIAL),banca));
-        ladoNorte.add(new Casilla("Serv2","Servicio",29,(0.75f*Valor.SUMA_VUELTA),banca));
-        ladoNorte.add(new Casilla("Solar17","Solar",30,(2.5f*Valor.FORTUNA_INICIAL),banca));
+        ladoNorte.add(new Casilla("Solar13","Solar",24,Valor.VALOR_GRUPO_VERMELLO/3,banca));
+        ladoNorte.add(new Casilla("Solar14","Solar",25,Valor.VALOR_GRUPO_VERMELLO/3,banca));
+        ladoNorte.add(new Casilla("Trans3","Transporte",26, (float) Valor.SUMA_VUELTA,banca));
+        ladoNorte.add(new Casilla("Solar15","Solar",27,Valor.VALOR_GRUPO_MARRON/3,banca));
+        ladoNorte.add(new Casilla("Solar16","Solar",28,Valor.VALOR_GRUPO_MARRON/3,banca));
+        ladoNorte.add(new Casilla("Serv2","Servicio",29, (float) (0.75f*Valor.SUMA_VUELTA),banca));
+        ladoNorte.add(new Casilla("Solar17","Solar",30,Valor.VALOR_GRUPO_MARRON/3,banca));
         posiciones.add(ladoNorte);
     }
 
@@ -43,15 +45,15 @@ public class Tablero {
     private void insertarLadoSur() {
         ArrayList<Casilla> ladoSur = new ArrayList<>();
         ladoSur.add(new Casilla("Salida","Especial", 1, banca));
-        ladoSur.add(new Casilla("Solar1","Solar",2,Valor.FORTUNA_INICIAL,banca));
+        ladoSur.add(new Casilla("Solar1","Solar",2,Valor.VALOR_GRUPO_NEGRO/2,banca));
         ladoSur.add(new Casilla("Caja","Comunidad",3,banca));
-        ladoSur.add(new Casilla("Solar2","Solar",4,Valor.FORTUNA_INICIAL,banca));
-        ladoSur.add(new Casilla("Imp1",5,Valor.SUMA_VUELTA,banca));
-        ladoSur.add(new Casilla("Trans1","Transporte",6,Valor.SUMA_VUELTA,banca));
-        ladoSur.add(new Casilla("Solar3","Solar",7,(1.3f*(Valor.FORTUNA_INICIAL)),banca));
+        ladoSur.add(new Casilla("Solar2","Solar",4,Valor.VALOR_GRUPO_NEGRO/2,banca));
+        ladoSur.add(new Casilla("Imp1",5, (float) Valor.SUMA_VUELTA,banca));
+        ladoSur.add(new Casilla("Trans1","Transporte",6, (float) Valor.SUMA_VUELTA,banca));
+        ladoSur.add(new Casilla("Solar3","Solar",7,Valor.VALOR_GRUPO_AZUL/3,banca));
         ladoSur.add(new Casilla("Suerte","Suerte",8,banca));
-        ladoSur.add(new Casilla("Solar4","Solar",9,(1.3f*(Valor.FORTUNA_INICIAL)),banca));
-        ladoSur.add(new Casilla("Solar5","Solar",10,(1.3f*(Valor.FORTUNA_INICIAL)),banca));
+        ladoSur.add(new Casilla("Solar4","Solar",9,Valor.VALOR_GRUPO_AZUL/3,banca));
+        ladoSur.add(new Casilla("Solar5","Solar",10,Valor.VALOR_GRUPO_AZUL/3,banca));
         posiciones.add(ladoSur);
     }
 
@@ -59,15 +61,15 @@ public class Tablero {
     private void insertarLadoOeste() {
         ArrayList<Casilla> ladoOeste = new ArrayList<>();
         ladoOeste.add(new Casilla("Carcel","Especial",11,banca));
-        ladoOeste.add(new Casilla("Solar6","Solar",12,(1.6f*Valor.FORTUNA_INICIAL),banca));
-        ladoOeste.add(new Casilla("Serv1","Servicio",13,(0.75f*Valor.SUMA_VUELTA),banca));
-        ladoOeste.add(new Casilla("Solar7","Solar",14,(1.6f*Valor.FORTUNA_INICIAL),banca));
-        ladoOeste.add(new Casilla("Solar8","Solar",15,(1.6f*Valor.FORTUNA_INICIAL),banca));
-        ladoOeste.add(new Casilla("Trans2","Transporte",16,Valor.SUMA_VUELTA,banca));
-        ladoOeste.add(new Casilla("Solar9","Solar",17,(1.9f*Valor.FORTUNA_INICIAL),banca));
+        ladoOeste.add(new Casilla("Solar6","Solar",12,Valor.VALOR_GRUPO_ROSA/3,banca));
+        ladoOeste.add(new Casilla("Serv1","Servicio",13, (float) (0.75f*Valor.SUMA_VUELTA),banca));
+        ladoOeste.add(new Casilla("Solar7","Solar",14,Valor.VALOR_GRUPO_ROSA/3,banca));
+        ladoOeste.add(new Casilla("Solar8","Solar",15,Valor.VALOR_GRUPO_ROSA/3,banca));
+        ladoOeste.add(new Casilla("Trans2","Transporte",16, (float) Valor.SUMA_VUELTA,banca));
+        ladoOeste.add(new Casilla("Solar9","Solar",17,Valor.VALOR_GRUPO_AMARELO/3,banca));
         ladoOeste.add(new Casilla("Caja","Comunidad",18,banca));
-        ladoOeste.add(new Casilla("Solar10","Solar",19,(1.9f*Valor.FORTUNA_INICIAL),banca));
-        ladoOeste.add(new Casilla("Solar11","Solar",20,(1.9f*Valor.FORTUNA_INICIAL),banca));
+        ladoOeste.add(new Casilla("Solar10","Solar",19,Valor.VALOR_GRUPO_AMARELO/3,banca));
+        ladoOeste.add(new Casilla("Solar11","Solar",20,Valor.VALOR_GRUPO_AMARELO/3,banca));
         ladoOeste.add(new Casilla("Parking","Especial",21,banca));
         posiciones.add(ladoOeste);
     }
@@ -76,24 +78,32 @@ public class Tablero {
     private void insertarLadoEste() {
         ArrayList<Casilla> ladoEste = new ArrayList<>();
         ladoEste.add(new Casilla("IrCarcel","Especial",31,banca));
-        ladoEste.add(new Casilla("Solar19","Solar",32,(2.8f*Valor.FORTUNA_INICIAL),banca));
-        ladoEste.add(new Casilla("Solar20","Solar",33,(2.8f*Valor.FORTUNA_INICIAL),banca));
-        ladoEste.add(new Casilla("Caja","Comunidad",34,banca));
-        ladoEste.add(new Casilla("Solar21","Solar",35,(2.8f*Valor.FORTUNA_INICIAL),banca));
-        ladoEste.add(new Casilla("Trans3","Transporte",36,Valor.SUMA_VUELTA,banca));
-        ladoEste.add(new Casilla("Suerte","Suerte",37,banca));
-        ladoEste.add(new Casilla("Solar22","Solar",38,(3.1f*Valor.FORTUNA_INICIAL),banca));
-        ladoEste.add(new Casilla("Imp2",39,(0.5f*Valor.SUMA_VUELTA),banca));
-        ladoEste.add(new Casilla("Solar23","Solar",40,(3.1f*Valor.FORTUNA_INICIAL),banca));
+        ladoEste.add(new Casilla("Solar18","Solar",32,Valor.VALOR_GRUPO_VERDE/3,banca));
+        ladoEste.add(new Casilla("Solar19","Solar",33,Valor.VALOR_GRUPO_VERDE/3,banca));
+        ladoEste.add(new Casilla("Solar20","Solar",34,Valor.VALOR_GRUPO_VERDE/3,banca));
+        ladoEste.add(new Casilla("Caja","Comunidad",35,banca));
+        ladoEste.add(new Casilla("Solar21","Solar",36,Valor.VALOR_GRUPO_AZUL_OSCURO/2,banca));
+        ladoEste.add(new Casilla("Trans3","Transporte",37, (float) Valor.SUMA_VUELTA,banca));
+        ladoEste.add(new Casilla("Suerte","Suerte",38,banca));
+        ladoEste.add(new Casilla("Solar22","Solar",39,Valor.VALOR_GRUPO_AZUL_OSCURO/2,banca));
+        ladoEste.add(new Casilla("Imp2",40, (float) (0.5f*Valor.SUMA_VUELTA),banca));
+
         posiciones.add(ladoEste);
     }
 
     //Para imprimir el tablero, modificamos el método toString().
     @Override
     public String toString() {
+
+        return " ";
     }
 
     //Método usado para buscar la casilla con el nombre pasado como argumento:
-    public Casilla encontrar_casilla(String nombre){
+
+    public Casilla encontrar_casilla(String nombr){
+
+        public static Casilla encontrar_casilla(String nombre){
+            return null;
+
+        }
     }
-}
