93b76b74800adac3016db9adbfb746489173a49d
daniiicb <danicervenak@gmail.com>
2024-10-05 22:21:30 +0200

    función hacerTirada optimizada

--- src/partida/Dado.java
+++ src/partida/Dado.java
@@ -6,22 +6,23 @@ import java.util.Random;
 public class Dado {
     //El dado solo tiene un atributo en nuestro caso: su valor.
     private int valor;
+    private final Random numRandom;
+    
+    public Dado(){
+        this.valor = 0;
+        this.numRandom = new Random(); 
+    }
 
     //Metodo para simular lanzamiento de un dado: devolverá un valor aleatorio entre 1 y 6.
     public int hacerTirada() {
-        Random numRandom = new Random();
         this.valor = numRandom.nextInt(6) + 1;
         return this.valor;
     }
 
 
-    //Getters y Setters
+    //Getters
 
     public int getValor(){
         return valor;
-    }
-
-    public void setValor(int valor){
-        this.valor = valor;
     }
 }
