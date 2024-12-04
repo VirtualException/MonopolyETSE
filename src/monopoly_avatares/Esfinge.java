451266e441e8bd0ab130abca94f64055994de71c
davidblanco00 <davidblanco000@gmail.com>
2024-12-04 19:01:12 +0100

    Implementamos la jerarquia de esfinge y sombrero

--- src/monopoly_avatares/Esfinge.java
+++ src/monopoly_avatares/Esfinge.java
@@ -1,4 +1,21 @@
 package monopoly_avatares;
 
-public class Esfinge {
+import monopoly_casillas.Casilla;
+import monopoly_exception.avatares.AvatarNoValidoException;
+import monopoly_juego.Juego;
+import monopoly_jugador.Jugador;
+import monopoly_tablero.Tablero;
+import monopoly_tablero.Valor;
+
+import java.util.ArrayList;
+
+public class Esfinge extends Avatar{
+    public Esfinge(Jugador jugador, Casilla lugar, ArrayList<Avatar> avCreados) throws AvatarNoValidoException {
+        super("Esfinge", jugador, lugar, avCreados);
+    }
+
+    @Override
+    public void moverEnAvanzado(Tablero tab, int tirada, ArrayList<Jugador> jugadores) {
+        Juego.consola.imprimir("Ester avatar es una esfinge, por lo que no tiene movimiento avanzado");
+    }
 }
--- src/monopoly_avatares/Sombrero.java
+++ src/monopoly_avatares/Sombrero.java
@@ -1,4 +1,20 @@
 package monopoly_avatares;
 
-public class Sombrero {
+import monopoly_casillas.Casilla;
+import monopoly_exception.avatares.AvatarNoValidoException;
+import monopoly_juego.Juego;
+import monopoly_jugador.Jugador;
+import monopoly_tablero.Tablero;
+
+import java.util.ArrayList;
+
+public class Sombrero extends Avatar{
+    public Sombrero(Jugador jugador, Casilla lugar, ArrayList<Avatar> avCreados) throws AvatarNoValidoException {
+        super("Sombrero", jugador, lugar, avCreados);
+    }
+
+    @Override
+    public void moverEnAvanzado(Tablero tab, int tirada, ArrayList<Jugador> jugadores) {
+        Juego.consola.imprimir("Ester avatar es una sombrero, por lo que no tiene movimiento avanzado");
+    }
 }
